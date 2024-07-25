03b93a2ad23a713b66a2cb406799b2e7e859ba9e
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-07-25 21:20:58 +0530

    hero section fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,12 +7,12 @@ const Hero = () => {
   return (
     <>
     <div className="md:flex m-4">
-        <div className=" md:max-w-[600px] md:max-h-[900px] mt-5">
+        <div className=" md:max-w-[615px] md:max-h-[900px] mt-5">
         <img src={heroImage} alt="Description" />
       </div>
-      <div className="ml-4 mr-4 md:mt-16 text sm:text-left text-center md:max-w-[755px] md:max-h-[658]">
+      <div className="ml-4 mr-4 md:mt-16 text sm:text-left text-center md:max-w-[670px] md:max-h-[658]">
         <h1 className="md:text-5xl text-4xl gradient-h1 poppins-regular g-h1">Joshua Kanatt</h1>
-        <p className='jost-400 text-xl leading-10 text-neutral-400 md:tracking-[3px] g-h2'>
+        <p className='jost-400 text-xl leading-10 text-neutral-400 md:tracking-[3px] g-h2 fc'>
         Founder & Chief Executive Officer - GAMERS TAG</p>     
         <p className='jost-400 mt-2 gradient-text md:tracking-widest hp'>
         I'm a visionary entrepreneur with over four years of expertise in Product management, architecture, Marketing, Data analytics, Business development & Consulting, and Startups. Designed over 100 world-class products, setting new industry standards. With unique talent for forecasting product growth, validated by extensive customer persona analysis. As an entrepreneur by heart, I bring a competitive mindset to product ownership,
@@ -21,7 +21,7 @@ assuring highest performance and innovation with mindset of 5x growth. Have a vi
         
         <br></br>
     <p className='md:text-2xl text-lg jost-400  bg-clip-text md:tracking-[3px] g-q'>"The journey of a thousand miles begins <br></br>with a single innovative step."</p>
-    <p className='md:mr-32 md:text-right text-center text-xl jost-400 g-q'>- Joshua Kanatt</p>
+    <p className='md:ml-14 md:text-right text-center text-xl jost-400 g-q'>- Joshua Kanatt</p>
     <div className="social-media-buttons md:mt-10 md:hidden">
     <a href="https://www.instagram.com/joshua_kanatt/" target="google.com" rel="noopener noreferrer">
       <FaLinkedin />
